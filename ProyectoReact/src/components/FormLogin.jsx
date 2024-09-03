b7bc19714d3c732a5b9848889f6a0dbe3fcdb0ee
unknown <aarguedas@fwdcostarica.com>
2024-09-03 11:23:00 -0600

    pagina Login completa

--- ProyectoReact/src/components/FormLogin.jsx
+++ ProyectoReact/src/components/FormLogin.jsx
@@ -4,7 +4,17 @@ import '../styles/FormLogin.css'
 function FormLogin() {
   return (
     <div className='divFormLogin'>
+        <h1>Estás en el formulario de login</h1>
 
+        <p>Ingresa tu número de identificación</p>
+        <input className='inputLogin' placeholder='Identificación' type="text" />
+
+        <p>Ingresa tu contraseña</p>
+        <input className='inputLogin' placeholder='Contraseña' type="password" name="" id="" />
+
+<br />
+<br />
+            <button className='botonLogin' type="button">INICIAR SESIÓN</button>
     </div>
   )
 }
